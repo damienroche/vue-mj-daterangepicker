67cc4f72c821b09105c7f31575c0486214efe8c8
Andreas Stephan <piccard@web.de>
2019-08-13 07:26:55 +0200

    Update index.js

--- src/translations/index.js
+++ src/translations/index.js
@@ -1,95 +1,95 @@
 const dictonnaries = {
-  fr: {
-    reset: 'Réinitialiser les dates',
-    submit: 'Valider',
-    previousMonth: 'Mois précédent',
-    nextMonth: 'Mois suivant',
-    previousYear: 'Année précédente',
-    nextYear: 'Année suivante',
-    quarter: 'Trimestre',
-    panels: {
-      range: 'Plage',
-      week: 'Semaine',
-      month: 'Mois',
-      quarter: 'Trimestre',
-      year: 'Année'
+  "fr": {
+    "reset": "Réinitialiser les dates",
+    "submit": "Valider",
+    "previousMonth": "Mois précédent",
+    "nextMonth": "Mois suivant",
+    "previousYear": "Année précédente",
+    "nextYear": "Année suivante",
+    "quarter": "Trimestre",
+    "panels": {
+      "range": "Plage",
+      "week": "Semaine",
+      "month": "Mois",
+      "quarter": "Trimestre",
+      "year": "Année"
     },
-    presets: {
-      custom: 'Période personnalisée',
-      forever: 'Depuis le début',
-      last7days: '7 derniers jours',
-      last30days: '30 derniers jours',
-      last90days: '90 derniers jours',
-      last365days: '365 dernier jours',
-      next365days: '365 prochains jours',
-      next90days: '90 prochains jours',
-      next30days: '30 prochains jours',
-      next7days: '7 prochains jours',
-      today: 'Aujourd\'hui',
-      tomorrow: 'Demain',
-      yesterday: 'Hier'
+    "presets": {
+      "custom": "Période personnalisée",
+      "forever": "Depuis le début",
+      "last7days": "7 derniers jours",
+      "last30days": "30 derniers jours",
+      "last90days": "90 derniers jours",
+      "last365days": "365 dernier jours",
+      "next365days": "365 prochains jours",
+      "next90days": "90 prochains jours",
+      "next30days": "30 prochains jours",
+      "next7days": "7 prochains jours",
+      "today": "Aujourd\"hui",
+      "tomorrow": "Demain",
+      "yesterday": "Hier"
     }
   },
-  en: {
-    reset: 'Reset ranges',
-    submit: 'Submit',
-    previousMonth: 'Previous month',
-    nextMonth: 'Next month',
-    previousYear: 'Previous year',
-    nextYear: 'Next year',
-    quarter: 'Quarter',
-    panels: {
-      range: 'Range',
-      week: 'Week',
-      month: 'Month',
-      quarter: 'Quarter',
-      year: 'Year'
+  "en": {
+    "reset": "Reset ranges",
+    "submit": "Submit",
+    "previousMonth": "Previous month",
+    "nextMonth": "Next month",
+    "previousYear": "Previous year",
+    "nextYear": "Next year",
+    "quarter": "Quarter",
+    "panels": {
+      "range": "Range",
+      "week": "Week",
+      "month": "Month",
+      "quarter": "Quarter",
+      "year": "Year"
     },
-    presets: {
-      custom: 'Custom range',
-      forever: 'From the beginning',
-      last7days: 'Last 7 days',
-      last30days: 'Last 30 days',
-      last90days: 'Last 90 days',
-      last365days: 'Last 365 days',
-      next365days: 'next 365 days',
-      next90days: 'Next 90 days',
-      next30days: 'Next 30 days',
-      next7days: 'Next 7 days',
-      today: 'Today',
-      tomorrow: 'Tomorrow',
-      yesterday: 'Yesterday'
+    "presets": {
+      "custom": "Custom range",
+      "forever": "From the beginning",
+      "last7days": "Last 7 days",
+      "last30days": "Last 30 days",
+      "last90days": "Last 90 days",
+      "last365days": "Last 365 days",
+      "next365days": "next 365 days",
+      "next90days": "Next 90 days",
+      "next30days": "Next 30 days",
+      "next7days": "Next 7 days",
+      "today": "Today",
+      "tomorrow": "Tomorrow",
+      "yesterday": "Yesterday"
     }
   },
-  de: {
-    reset: 'Zurücksetzen',
-    submit: 'Ok',
-    previousMonth: 'Letztes Monat',
-    nextMonth: 'Nächstes Monat',
-    previousYear: 'Letztes Jahr',
-    nextYear: 'Nächstes Jahr',
-    quarter: 'Quartal',
-    panels: {
-      range: 'Zeitraum',
-      week: 'Woche',
-      month: 'Monat',
-      quarter: 'Quartal',
-      year: 'Jahr'
+  "de": {
+    "reset": "Zurücksetzen",
+    "submit": "Ok",
+    "previousMonth": "Letztes Monat",
+    "nextMonth": "Nächstes Monat",
+    "previousYear": "Letztes Jahr",
+    "nextYear": "Nächstes Jahr",
+    "quarter": "Quartal",
+    "panels": {
+      "range": "Zeitraum",
+      "week": "Woche",
+      "month": "Monat",
+      "quarter": "Quartal",
+      "year": "Jahr"
     },
-    presets: {
-      custom: 'Benutzerdefiniert',
-      forever: 'Von Anfang',
-      last7days: 'Letzten 7 Tage',
-      last30days: 'Letzten 30 Tage',
-      last90days: 'Letzten 90 Tage',
-      last365days: 'Letzten 365 Tage',
-      next365days: 'Nächsten 365 Tage',
-      next90days: 'Nächsten 90 Tage',
-      next30days: 'Nächsten 30 Tage',
-      next7days: 'Nächsten 7 Tage',
-      today: 'Heute',
-      tomorrow: 'Morgen',
-      yesterday: 'Gestern'
+    "presets": {
+      "custom": "Benutzerdefiniert",
+      "forever": "Von Anfang",
+      "last7days": "Letzten 7 Tage",
+      "last30days": "Letzten 30 Tage",
+      "last90days": "Letzten 90 Tage",
+      "last365days": "Letzten 365 Tage",
+      "next365days": "Nächsten 365 Tage",
+      "next90days": "Nächsten 90 Tage",
+      "next30days": "Nächsten 30 Tage",
+      "next7days": "Nächsten 7 Tage",
+      "today": "Heute",
+      "tomorrow": "Morgen",
+      "yesterday": "Gestern"
     }
   }
 }
